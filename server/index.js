b5a8eafc9a63f222b90b796a338c0ccd3487f708
Abhishek280999 <darwekar155@gmail.com>
2023-08-26 22:12:44 +0530

    done

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080
 const schemaData = mongoose.Schema({
     name : String,
     email : String,
-    mobile : Number,
+    mobile : String,
 },{
     timestamps : true
 })
@@ -24,6 +24,7 @@ const schemaData = mongoose.Schema({
 const userModel = mongoose.model("user",schemaData)
 
 //read data
+//  http://localhost:8080/
 app.get('/', async (req, res) => {
     const data = await userModel.find({})
 
@@ -31,30 +32,45 @@ app.get('/', async (req, res) => {
 })
 
 //create data  || save data
+//  http://localhost:8080/create
+/*
+    name : ,
+    email : ,
+    mobile : ,
+*/
 app.post('/create', async(req,res)=>{
     console.log(req.body)
     const data = new userModel(req.body)
     await data.save( )
-    res.send({success : true , message : "Data save ",data : data})
+    res.send({success : true , message : "Data save successfully ",data : data})
 })
 
 //update data
+// http://localhost:8080/update
+
+/*
+    id : 
+    name : ,
+    email : ,
+    mobile : ,
+*/
 app.put("/update", async (req ,res)=>{
     console.log(req.body)
-    const {id ,...rest} = req.body
+    const {_id ,...rest} = req.body
 
     console.log(rest)
-    const data = await  userModel.updateOne({_id : id},rest)
-    res.send({success : true , message : "Data will updated ",data : data})
+    const data = await  userModel.updateOne({_id : _id},rest)
+    res.send({success : true , message : "Data  updated successfully ",data : data})
 
 })
 
 //delete
+// http://localhost:8080/delete/id
 app.delete("/delete/:id", async (req ,res)=>{
    const id = req.params.id
    console.log(id)
    const data = await userModel.deleteOne({_id : id})
-   res.send({success : true , message : "Data will deleted ",data : data})
+   res.send({success : true , message : "Data  deleted successfully ",data : data})
 
 })
 
@@ -63,7 +79,7 @@ app.delete("/delete/:id", async (req ,res)=>{
 mongoose.connect("mongodb://127.0.0.1/crudoperation")
     .then(() => {
         console.log("connect to DB")
-        app.listen(PORT, () => console.log("server is running"))
+        app.listen(PORT, () => console.log("server is running successfully"))
 
     })
     .catch((err) => console.log(err))
